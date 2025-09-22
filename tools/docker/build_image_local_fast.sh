db104bd2968b794e9aa381b9b53b6f922158074e
bronzeMe <1224533208@qq.com>
2025-09-22 12:00:08 +0800

    fix tools/docker/build_image_local_fast.sh, remove mlsdisk patch from Makefile

--- tools/docker/build_image_local_fast.sh
+++ tools/docker/build_image_local_fast.sh
@@ -45,5 +45,5 @@ check_item_in_list "$os_name" "ubuntu22.04" || report_error
 
 # 切换到Occlum根目录，确保Docker构建上下文正确
 cd "$script_dir/../.."
-docker build --no-cache-f "$script_dir/Dockerfile.$os_name-local-fast" -t "occlum/occlum:$occlum_label-$os_name-local-fast" .
+docker build --no-cache -f "$script_dir/Dockerfile.$os_name-local-fast" -t "occlum/occlum:$occlum_label-$os_name-local-fast" .
 
